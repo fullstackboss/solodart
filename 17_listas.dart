7fd87e61e1d4dd85d80a5183bb517220fef6a4a3
BigBoss Snake <romeltejada@gmail.com>
2023-06-20 21:55:54 -0500

    Dia 2: en Dart

---
+++ 17_listas.dart
@@ -0,0 +1,9 @@
+void main() {
+  //Lista de Guerreros String
+  List<String> guerreros = ["goku", "vegeta", "bardock", "broly", "gohan"];
+  print(guerreros);
+
+  //Lista de Numeros
+  var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  print(numeros);
+}
